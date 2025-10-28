86f6c82d1cb4bee260a4b335b599d9f8ce480444
Jefferson Le <jnl9695@nyu.edu>
2025-10-28 16:14:47 -0400

    more updates to endpoints

--- server/endpoints.py
+++ server/endpoints.py
@@ -2,13 +2,11 @@
 This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
-# from http import HTTPStatus
+from http import HTTPStatus
 
 from flask import Flask
-from flask_restx import Resource, Api, reqparse  # , fields  # Namespace
+from flask_restx import Resource, Api, reqparse
 from flask_cors import CORS
-
-# import werkzeug.exceptions as wz
 import cities.cities as ct
 
 app = Flask(__name__)
@@ -42,6 +40,9 @@ population_put = reqparse.RequestParser()
 population_put.add_argument('city_id', type=str, required=True)
 population_put.add_argument('population', type=int, required=True)
 
+city_delete = reqparse.RequestParser()
+city_delete.add_argument('city_id', type=str, required=True)
+
 
 @api.route(f'{CITIES_EPS}')
 class Cities(Resource):
@@ -50,7 +51,8 @@ class Cities(Resource):
             cities = ct.read()
             return {CITIES_RESP: cities, "Number of cities": len(cities)}
         except ConnectionError:
-            return {ERROR: "There is a connection error"}, 500
+            return {ERROR: "There is a connection error"}, \
+                HTTPStatus.INTERNAL_SERVER_ERROR
 
     @api.expect(city_post)
     def post(self):
@@ -62,8 +64,8 @@ class Cities(Resource):
         }
         try:
             ct.create(city)
-        except ValueError:
-            return {ERROR: "There is a value error"}, 400
+        except ValueError as e:
+            return {ERROR: str(e)}, HTTPStatus.BAD_REQUEST
         return {SUCCESS: True}
 
     @api.expect(population_put)
@@ -71,10 +73,26 @@ class Cities(Resource):
         args = population_put.parse_args()
         city_id = args['city_id']
         population = args['population']
+
+        # Validate population is not negative
+        if population < 0:
+            return {ERROR: "Population cannot be negative"}, \
+                HTTPStatus.BAD_REQUEST
+
         try:
             ct.set_population(city_id, population)
-        except ValueError:
-            return {ERROR: "There is a value error"}, 400
+        except ValueError as e:
+            return {ERROR: str(e)}, HTTPStatus.BAD_REQUEST
+        return {SUCCESS: True}
+
+    @api.expect(city_delete)
+    def delete(self):
+        args = city_delete.parse_args()
+        city_id = args['city_id']
+        try:
+            ct.delete(city_id)
+        except ValueError as e:
+            return {ERROR: str(e)}, HTTPStatus.BAD_REQUEST
         return {SUCCESS: True}
 
 
@@ -104,3 +122,23 @@ class Endpoints(Resource):
             rule.rule for rule in api.app.url_map.iter_rules()
         )
         return {"Available endpoints": endpoints}
+
+
+@api.route(f'{CITIES_EPS}/<string:city_id>')
+class City(Resource):
+    """
+    Endpoint for operations on individual cities.
+    """
+    def get(self, city_id):
+        """
+        Get a specific city by ID.
+        """
+        try:
+            cities = ct.read()
+            if city_id not in cities:
+                return {ERROR: f"City {city_id} not found"}, \
+                    HTTPStatus.NOT_FOUND
+            return {CITIES_RESP: cities[city_id]}
+        except ConnectionError:
+            return {ERROR: "There is a connection error"}, \
+                HTTPStatus.INTERNAL_SERVER_ERROR
