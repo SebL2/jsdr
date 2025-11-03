7f5fcb78f12afd674cad899b2ec89b3998ac96e8
“dye”6601” <dyk6601@gmail.com>
2025-11-03 15:06:18 -0500

    Revert "added @needs_db decorator to handle MongoDB connections automatically"
    This reverts commit 08a360862db5df15418549d131f5ea999623a581.

--- server/db_connect.py
+++ server/db_connect.py
@@ -1,27 +1,11 @@
 # server/db_connect.py
 
-from functools import wraps
-from pymongo import MongoClient
-
-client = None
-db = None
-
-
-def connect_to_mongo():
-    """Connect to MongoDB and return a database handle."""
-    global client, db
-    if not client:
-        client = MongoClient("mongodb://localhost:27017/")
-        db = client["geodata"]
-    return db
-
-
-def needs_db(func):
-    """Decorator to ensure a MongoDB connection before running the function."""
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        global db
-        if db is None:
-            db = connect_to_mongo()
-        return func(*args, **kwargs)
-    return wrapper
+class DBConnect:
+    def connect(self):
+        """
+        Simulates connecting to a database.
+        In a real implementation, this would initialize a MongoDB
+        or SQL connection.
+        """
+        print("Connecting to database...")
+        return True
